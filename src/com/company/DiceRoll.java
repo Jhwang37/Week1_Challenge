ce70a5f5074b09db591a95842d2fd9ce0c93af80
jhwang37 <jhwang37@gmail.com>
2020-01-24 14:51:59 -0500

    second commit

--- src/com/company/DiceRoll.java
+++ src/com/company/DiceRoll.java
@@ -13,23 +13,28 @@ public class DiceRoll  {
         String keepPlaying = "";
 
         System.out.println("Welcome! Let's Play Roll Dice!");
-
-        while (!keepPlaying.equalsIgnoreCase("no")){
+        while (!keepPlaying.equalsIgnoreCase("no")) {
             rollDice1 = random.nextInt((6 - 1) + 1) + 1;//random with 1-6 range
             rollDice2 = random.nextInt((6 - 1) + 1) + 1;
             System.out.println(rollDice1 + " and " + rollDice2);
+
             if (rollDice1 == 1 || rollDice2 == 1) { // add 0 if either of the rolls are 1
                 total += 0;
                 System.out.println("Score: " + total);
                 System.out.println("Roll again? ");
                 keepPlaying = scan.nextLine();
-            } else if(rollDice1 == 1 && rollDice2 == 1){//add 25 total if both rolls are 1
-                total += 25;
-            }else if(total >= 100){//end loop
+            }
+            if (rollDice1 == 1 && rollDice2 == 1) {//add 25 total if both rolls are 1
+                total = total + 25;
+                System.out.println("Score: " + total);
+                System.out.println("Roll again? ");
+                keepPlaying = scan.nextLine();
+            }
+            if (total >= 100) {//end loop
                 System.out.println("Congrats! You won!");
                 break;
-            } else{ //add dice rolls to total
-                total += (rollDice1+rollDice2);
+            } else if(!(rollDice1 == 1) && !(rollDice2 == 1)) { //add dice rolls to total
+                total += (rollDice1 + rollDice2);
                 System.out.println("Score: " + total);
                 System.out.println("Roll again? ");
                 keepPlaying = scan.nextLine();
